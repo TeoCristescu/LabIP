7b4c94144f966d243c733c327ff8d78454052cea
TeoCristescu <teocrriss99@gmail.com>
2020-10-26 09:33:50 +0200

    aaa utilizator
    Signed-off-by: TeoCristescu <teocrriss99@gmail.com>

--- Proiect1/src/ro/mta/facc/selab/Utilizator.java
+++ Proiect1/src/ro/mta/facc/selab/Utilizator.java
@@ -1,4 +1,14 @@
 package ro.mta.facc.selab;
 
+import java.util.ArrayList;
+
 public class Utilizator {
+
+    String nume;
+    ArrayList<Carte> cartiImprumutate;
+
+    public Utilizator(String Nume)
+    {
+        this.nume=Nume;
+    }
 }
--- src/ro/mta/facc/selab/MyThread.java
+++
@@ -1,8 +0,0 @@
-package ro.mta.facc.selab;
-
-public class MyThread extends Thread{
-    @Override
-    public void run() {
-        System.out.println("Hellof from "+Thread.currentThread().getName());
-    }
-}
